b6e1912210bd74dc68c9f306bdf1b31bf737808d
Jhonattan Lino <jhonattanlino@outlook.com>
2023-10-22 22:58:11 -0300

    code: update data

--- ML-Model/main.py
+++ ML-Model/main.py
@@ -2,5 +2,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from ucimlrepo import fetch_ucirepo
+from sklearn.model_selection import train_test_split # pylint: disable=unused-import
 from sklearn.metrics import confusion_matrix, accuracy_score
+
+# %% Carregando o dataset
+# fetch dataset
+statlog_german_credit_data = fetch_ucirepo(id=144)
+
+# data (as pandas dataframes) 
+X = statlog_german_credit_data.data.features
+y = statlog_german_credit_data.data.targets
+# %%
