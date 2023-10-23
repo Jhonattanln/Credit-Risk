7e8427635823c83363c4328ee0c1613314251473
Jhonattan Lino <jhonattanlino@outlook.com>
2023-10-22 23:13:54 -0300

    code: Initing data transform

--- ML-Model/main.py
+++ ML-Model/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from ucimlrepo import fetch_ucirepo
-from sklearn.model_selection import train_test_split # pylint: disable=unused-import
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 # %% Carregando o dataset
@@ -13,4 +13,6 @@ statlog_german_credit_data = fetch_ucirepo(id=144)
 # data (as pandas dataframes) 
 X = statlog_german_credit_data.data.features
 y = statlog_german_credit_data.data.targets
-# %%
+
+# %% Tratamento dos dados
+
